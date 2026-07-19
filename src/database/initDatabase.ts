b6c905682943ef1810905b84118a4d17bb218935
Osama <abdelahman.osos12321@gmail.com>
2026-07-19 21:00:24 +0300

    msg

--- src/database/initDatabase.ts
+++ src/database/initDatabase.ts
@@ -1,8 +1,7 @@
 import database from "../config/database";
 import sequelize from "../config/database";
 import "./associations";
-import JobOrder from "../modules/jobOrder/model";
-import JobOrderLine from "../modules/jobOrderLine/model";
+
 
 import Customer from "../modules/customer/model";
 
